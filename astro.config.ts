2f462a6ee85be75cfdcce3fb0483173f30781c96
alex <nowaalex@gmail.com>
2024-05-02 13:53:35 +0200

    removed devtoolbar

--- astro.config.ts
+++ astro.config.ts
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [tailwind(), vercel()],
 });
