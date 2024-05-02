cd9e59f28dbfa48035c0684c3174e54eed9d0940
alex <nowaalex@gmail.com>
2024-05-02 07:27:17 +0200

    alpha version

---
+++ astro.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "hybrid",
+  integrations: [tailwind(), vercel()],
+});
